ff66bac559ec6df092cc048496ac99efff150e17
NiteshTembe <123352437+NiteshTembe@users.noreply.github.com>
2023-02-25 23:39:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.title="Promise Task 2"
 
-async function getFishData(){
+// This function is used to fetch data usin api
+async function getData(){
     try{
         let res = await fetch("https://api.coincap.io/v2/assets")
         let data = await res.json()
@@ -11,26 +12,29 @@ async function getFishData(){
         console.log(err)
     }
 }
-
+// h1 tag with id title created here
 const title= document.createElement("h1")
 title.classList.add("text-center")
 title.id="title"
+// text for h1 tag created
 const titletext = document.createTextNode("Real Time Cryptocurrency Data")
 title.appendChild(titletext)
 document.body.appendChild(title)
+// container div created here
 const container = document.createElement("div")
 container.classList.add("container")
+//div for responsive table created here
 const tableDiv = document.createElement("div")
 tableDiv.classList.add("table-responsive")
 tableDiv.id="tableDiv"
 container.appendChild(tableDiv)
 document.body.appendChild(container)
-
-getFishData()
+//getdata function is called here to get and show data in html document
+getData()
 
 function showData(data){
     let tablediv = document.getElementById("tableDiv")
-    console.log(data)
+// inner html for id table div is here
     let tabledata = `
     <table class="table" id="table">
         <thead class="text-center text-bg-primary">
